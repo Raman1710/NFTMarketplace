d27dc768c1df6fbcf1e32ba9b86998c9ae49deeb
Ramandeep Singh Saini <sainiramandeep1710@gmail.com>
2025-01-28 21:18:01 +0530

    Add Avatar and Tabs components; update UserProfile and ArtistProfile pages

--- client/src/Pages/ArtistProfile.jsx
+++ client/src/Pages/ArtistProfile.jsx
@@ -1,9 +1,81 @@
-import React from 'react'
+import Profile from '@/Components/Profile/Profile.jsx';
+import SocialLinks from '@/Components/SocialLinks/SocialLinks.jsx';
+import CoverImage from '@/assets/cover.svg'
+import AvatarImage from '@/assets/userAvatar.svg'
+import ProfileTabs from '@/Components/ProfileTabs/ProfileTabs';
+
 
 const ArtistProfile = () => {
-  return (
-    <div>ArtistProfile</div>
-  )
-}
+  const artist = {
+    name: 'CryptoArtEnthusiast23',
+    avatar: AvatarImage,
+    cover: CoverImage,
+    bio: '🌐 Exploring the decentralized realm of digital art has been an exhilarating experience for me. I believe in the power of blockchain to revolutionize the art industry, empowering artists and collectors alike. Every NFT I own is not just a piece of art but a unique token representing the future of artistic expression.',
+    stats: { nftsSold: 30, totalEarnings: 12.3 },
+    socialLinks: [
+      { platform: 'Twitter', url: 'https://twitter.com/jane_smith' },
+      { platform: 'Instagram', url: 'https://instagram.com/jane_smith' },
+    ],
+    followers: '23K',
+    following: '25',
+  };
 
-export default ArtistProfile
+  const artistTabs = [
+    {
+      label: "Owned",
+      value: "owned",
+      content: <div>Artist's Portfolio</div>,
+    },
+    {
+      label: "On sale",
+      value: "sale",
+      content: <div>Sales Data</div>,
+    },
+    {
+      label: "Collections",
+      value: "collection",
+      content: <div>Links to artist's social media</div>,
+    },
+    {
+      label: "Created",
+      value: "created",
+      content: <div>Links to created</div>,
+    },
+    {
+      label: "Activity",
+      value: "activity",
+      content: <div>Links to activity</div>,
+    },
+    {
+      label: "More",
+      value: "more",
+      content: <div>Links to more</div>,
+    },
+  ];
+
+
+  return (
+    <>
+
+      <Profile
+        name={artist.name}
+        avatar={artist.avatar}
+        cover={artist.cover}
+        bio={artist.bio}
+        stats={artist.stats}
+
+      >
+        <SocialLinks
+          links={artist.socialLinks}
+          followers={artist.followers}
+          following={artist.following} />
+      </Profile>
+
+      <div className="p-4">
+        <ProfileTabs tabs={artistTabs} />
+      </div>
+    </>
+  );
+};
+
+export default ArtistProfile;
--- client/src/Pages/UserProfile.jsx
+++ client/src/Pages/UserProfile.jsx
@@ -1,9 +1,72 @@
-import React from 'react'
+import Profile from '@/Components/Profile/Profile.jsx';
+import CoverImage from '@/assets/cover.svg'
+import AvatarImage from '@/assets/userAvatar.svg'
+import ProfileTabs from '@/Components/ProfileTabs/ProfileTabs';
 
 const UserProfile = () => {
-  return (
-    <div>UserProfile</div>
-  )
-}
+  const user = {
+    name: 'CryptoArtEnthusiast23',
+    avatar: AvatarImage,
+    cover: CoverImage,
+    bio: '🌐 Exploring the decentralized realm of digital art has been an exhilarating experience for me. I believe in the power of blockchain to revolutionize the art industry, empowering artists and collectors alike. Every NFT I own is not just a piece of art but a unique token representing the future of artistic expression.',
+    stats: { nftsSold: 12, totalEarnings: 4.5 },
+  };
 
-export default UserProfile
+  const userTabs = [
+    {
+      label: "Collected",
+      value: "collected",
+      content: <div>User Overview Content</div>,
+    },
+    {
+      label: "Offers made",
+      value: "offers",
+      content: <div>List of NFTs owned by the user</div>,
+    },
+    {
+      label: "Deals",
+      value: "deals",
+      content: <div>deals Settings</div>,
+    },
+    {
+      label: "Created",
+      value: "created",
+      content: <div>created Settings</div>,
+    },
+    {
+      label: "Favorited",
+      value: "favorited",
+      content: <div>favorited Settings</div>,
+    },
+    {
+      label: "Activity",
+      value: "activity",
+      content: <div>activity Settings</div>,
+    },
+    {
+      label: "More",
+      value: "more",
+      content: <div>more Settings</div>,
+    },
+  ];
+
+
+  return (
+    <>
+    <Profile
+      name={user.name}
+      avatar={user.avatar}
+      cover={user.cover}
+      bio={user.bio}
+      stats={user.stats}
+    />
+
+     <div className="p-4">
+      <ProfileTabs tabs={userTabs} />
+    </div>
+
+    </>
+  );
+};
+
+export default UserProfile;
